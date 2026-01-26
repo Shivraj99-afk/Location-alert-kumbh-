a01b8dbb0440f6a9b01cc42938b8db58e25cd471
Shivraj <shivrajpawar6906@gmail.com>
2026-01-26 23:33:04 +0530

    added bot

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,6 +26,27 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased selection:bg-blue-500/30`}
       >
         {children}
+        <Script id="botsonic-widget" strategy="afterInteractive">
+          {`
+            (function (w, d, s, o, f, js, fjs) {
+              w["botsonic_widget"] = o;
+              w[o] =
+                w[o] ||
+                function () {
+                  (w[o].q = w[o].q || []).push(arguments);
+                };
+              (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+              js.id = o;
+              js.src = f;
+              js.async = 1;
+              fjs.parentNode.insertBefore(js, fjs);
+            })(window, document, "script", "Botsonic", "https://widget.botsonic.com/CDN/botsonic.min.js");
+            Botsonic("init", {
+              serviceBaseUrl: "https://api-bot.writesonic.com",
+              token: "0ee50a95-5d03-4ece-bd73-1f9935764f87",
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
